d7af93f501a20a1f00e4a2b177614a105983b594
Dev-gojoy <32jongwoo@gmail.com>
2023-03-30 11:49:57 +0900

    tailwind_install

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
-      <h2>My Name is Ko Jong Woo</h2>
-      <h3>My Favorite drink is Sprite </h3>
+      <h1 className="text-3xl font-bold underline">Hello, React!</h1>
+      {/* 자바스크립트 리액트 문법은 className을 사용한다. */}
+      <h2 className="text-3xl font-bold underline">My Name is Ko Jong Woo</h2>
+      <h3 className="text-3xl font-bold underline">
+        My Favorite drink is Sprite 🥤
+      </h3>
     </div>
   );
 }

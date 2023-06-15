f40672c292f751573478a99cab336bdd3d33155f
Melonisjy <dhy2457@naver.com>
2023-06-16 00:48:20 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello, React!</h1>
-      {/* 자바스크립트 리액트 문법은 className을 사용한다. */}
-      <h2 className="text-3xl font-bold underline">My Name is Ko Jong Woo</h2>
-      <h3 className="text-3xl font-bold underline">
-        My Favorite drink is Sprite 🥤
-      </h3>
+      <h3>test</h3>
     </div>
   );
 }

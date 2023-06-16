203033fffe389febb217513460fa5e7caffde66b
Melonisjy <dhy2457@naver.com>
2023-06-16 15:14:27 +0900

    ✨ [feat] 헤더 추가(라우팅, 로고 이미지 등 추가 작업 해야함)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Header = ({ account, setAccount }) => {
   const onClickConnect = async () => {
     if (window.ethereum) {
@@ -25,27 +27,90 @@ const Header = ({ account, setAccount }) => {
     }
   };
   return (
-    <header className="py-4 px-8 flex justify-end">
-      <div className="flex items-center">
-        {account ? (
-          <div className="flex items-center">
-            <div className="">
-              {account.substring(0, 4)}....
-              {account.substring(account.length - 4)}
+    <header>
+      <nav className="bg-white px-4 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link className="flex items-center" to="/">
+            <div className="justify-between flex">
+              <div className="mr-3 h-6">logo img</div>
+              <span class="self-center text-xl font-semibold">DPI</span>
             </div>
-            <button
-              className="p-2 ml-4 bg-gray-200 rounded-xl"
-              onClick={onClickDisconnect}
-            >
-              disconnect
-            </button>
+          </Link>
+          <div className="p-2 flex">
+            {account ? (
+              <div className="justify-between items-center w-full">
+                <ul className="flex flex-row font-medium">
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">About DPI</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">
+                      Buy Specialized Covers
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">NFT Minting</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">Governance</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">FAQ</Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <div className="justify-between items-center w-full">
+                <ul className="flex flex-row font-medium">
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">About DPI</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">
+                      Buy Specialized Covers
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">NFT Minting</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">Governance</Link>
+                  </li>
+                  <li>
+                    <Link className="block py-2 pr-4 pl-3">FAQ</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
-        ) : (
-          <button className="flex items-center" onClick={onClickConnect}>
-            Connect Wallet
-          </button>
-        )}
-      </div>
+          <div className="flex items-center">
+            {account ? (
+              <div className="flex items-center">
+                <div className="">
+                  {account.substring(0, 4)}....
+                  {account.substring(account.length - 4)}
+                </div>
+                <button
+                  className="p-2 ml-4 bg-gray-200 rounded-xl"
+                  onClick={onClickDisconnect}
+                >
+                  disconnect
+                </button>
+              </div>
+            ) : (
+              <button
+                className="bg-gray-200 p-2 rounded-xl"
+                onClick={onClickConnect}
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };

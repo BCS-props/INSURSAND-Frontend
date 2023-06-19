fe8b1e53d1d89e87ec430a9bfe45ea17e10f80fc
Melonisjy <dhy2457@naver.com>
2023-06-19 23:51:43 +0900

    ✨ [feat] main page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,10 @@
 const Main = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      main page
+    <div className="flex justify-center min-h-screen bg-gradient-to-b from-white to-stone-400 pt-32">
+      <div className="text-6xl font-bold text-stone-600/75">
+        <div className="">Every moment in DeFi,</div>
+        <div className="mt-12">safe easy with DPI</div>
+      </div>
     </div>
   );
 };

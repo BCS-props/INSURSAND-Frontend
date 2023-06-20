479b55c4c09a06730cfd1d34d7daaeab9934108b
Melonisjy <dhy2457@naver.com>
2023-06-20 23:24:11 +0900

    ✨ [feat] create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="h-80 bg-amber-800 opacity-50">
+      <div>
+        <div className="pt-16 pl-12">
+          <div className="flex w-fit items-center">
+            <div>logo img</div>
+            <div className="text-4xl ml-4">S A N D e F I</div>
+          </div>
+          <div className="mt-32">
+            <div>discord</div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default Footer;

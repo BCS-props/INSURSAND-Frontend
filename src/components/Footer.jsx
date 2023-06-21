ee5dee303620a904b28cb896bd91a40cc7441243
Melonisjy <dhy2457@naver.com>
2023-06-21 17:52:25 +0900

    🔧 [config] changed style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <div className="h-80 bg-amber-800 opacity-50">
+    <div className="bg-amber-800 opacity-50">
       <div>
-        <div className="pt-16 pl-12">
+        <div className="pt-16 pl-12 pb-12 ml-4">
           <div className="flex w-fit items-center">
             {/* <div>logo img</div> */}
-            <div className="text-4xl ml-4">I N S U R S A N D</div>
+            <div className="text-4xl">I N S U R S A N D</div>
           </div>
-          <div className="mt-32">
+          <div className="pt-12">
             <div>discord</div>
           </div>
         </div>

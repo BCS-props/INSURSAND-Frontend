8c1b1eaf6a8e60ff7c99349d397d44b0e5e4041d
Melonisjy <dhy2457@naver.com>
2023-06-24 01:59:34 +0900

    ✨ [feat] design & add status icon & add totalVotes

--- src/components/Proposals.jsx
+++ src/components/Proposals.jsx
@@ -1,27 +1,38 @@
-const Proposals = ({ subject, status }) => {
+import { FaCircle, FaCheck } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Proposals = ({ subject, status, accept, deny }) => {
+  let statusIcon;
   const getStatus = () => {
     if (status === 0) {
-      status = "in Progress";
+      statusIcon = <FaCircle className="text-yellow-300" />;
+      status = "In Progress";
     } else if (status === 1) {
-      status = "passed";
+      statusIcon = <FaCheck className="text-green-300" />;
+      status = "Proposed";
     } else if (status === 2) {
-      status = "rejected";
+      statusIcon = <AiOutlineClose className="text-red-600" />;
+      status = "Rejected";
     }
   };
   getStatus();
+
+  const totalVotes = accept + deny; // 총 투표수
+
   return (
     <div className="px-12">
-      <div className="bg-amber-600/80 rounded-xl hover:bg-amber-600/40 duration-200">
-        <div className="p-4">
-          <div className="flex justify-between">
-            <div className="">{subject}</div>
-            <div>{status}</div>
-          </div>
-          <div className="text-xs mt-2 opacity-50">
-            {/* votes: {Number(v.accept)} + {Number(v.deny)} */}
+      <button className="bg-amber-600/80 rounded-xl hover:bg-amber-600/40 duration-200 min-w-full p-4">
+        <div className="flex justify-between">
+          <div className="text-xl">{subject}</div>
+          <div className="pt-4 flex items-center gap-2">
+            <div className="">{statusIcon}</div>
+            <div className="text-lg">{status}</div>
           </div>
         </div>
-      </div>
+        <div className="text-sm opacity-50 justify-start flex w-fit">
+          total votes: {totalVotes}
+        </div>
+      </button>
     </div>
   );
 };

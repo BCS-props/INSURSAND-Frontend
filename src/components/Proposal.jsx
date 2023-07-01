df364dc4ed21c8682e4c1dd8c630959204ac090b
Melonisjy <dhy2457@naver.com>
2023-07-02 01:34:25 +0900

    ✨ [feat] pagination

--- src/components/Proposal.jsx
+++ src/components/Proposal.jsx
@@ -9,6 +9,10 @@ const Proposal = () => {
   const [proposalNum, setProposalNum] = useState();
   const [proposalInfo, setProposalInfo] = useState();
 
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
   const web3 = new Web3(`https://goerli.infura.io/v3/${apiKey}`);
   const GVN_contract = new web3.eth.Contract(GOVERNANCE_ABI, GOVERNANCE_CA);
 
@@ -38,6 +42,27 @@ const Proposal = () => {
     getProposalData();
   }, [proposalNum]);
 
+  const renderPageButtons = () => {
+    const totalPages = Math.ceil(proposalNum / limit);
+    const buttons = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <button
+          key={i}
+          onClick={() => setPage(i)}
+          className={`mx-1 py-1 px-2 rounded-lg ${
+            i === page ? "bg-amber-700 text-white" : "bg-white text-amber-700"
+          }`}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    return buttons;
+  };
+
   return (
     <div className="min-h-screen flex justify-center bg-gradient-to-r from-amber-400/80 to-amber-600/80 pt-14 pb-20">
       <div className="mx-96 w-1/2 mt-20 min-h-screen bg-white rounded-xl shadow-2xl">
@@ -60,7 +85,7 @@ const Proposal = () => {
         </div>
         <div className="grid gap-8 mx-12">
           {proposalInfo &&
-            proposalInfo.map((v, i) => {
+            proposalInfo.slice(offset, offset + limit).map((v, i) => {
               return (
                 <Proposals
                   key={i}
@@ -72,6 +97,9 @@ const Proposal = () => {
               );
             })}
         </div>
+      </div>
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 font-bold">
+        {renderPageButtons()}
       </div>
     </div>
   );
